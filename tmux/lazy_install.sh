515f3387c9bf8443da4f235bbc789c78a1e972d9
Sam Xie <xsambundy@gmail.com>
2019-02-04 10:08:55 +0800

    build and install tmux from a release tarball

--- tmux/lazy_install.sh
+++ tmux/lazy_install.sh
@@ -4,10 +4,24 @@ set -euxo pipefail
 # For tmux
 sudo apt install -y autotools-dev automake libevent-dev ncurses-dev
 # Required tmux 2.6 or later version
-sudo apt install -y tmux
+#sudo apt install -y tmux
+
+# Install tmux 2.8
+TMUX_VERSION='2.8'
+
+cd /tmp
+curl -L  https://github.com/tmux/tmux/releases/download/$TMUX_VERSION/tmux-$TMUX_VERSION.tar.gz -o tmux-$TMUX_VERSION.tar.gz
+tar zxvf tmux-$TMUX_VERSION.tar.gz
+cd tmux-$TMUX_VERSION
+./configure && make
+sudo make install
 
 # Install Tmux Plugin Manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Set conf
 cp .tmux.conf ~/.tmux.conf
+
+set +x
+echo 'After enter tmux, press <prefix> + I to install plugins.'
+echo 'Enjoy!'
