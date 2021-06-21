ea6094737503f16a184de45c35adff772f1d9cbb
Sam Xie <xsambundy@gmail.com>
2021-06-21 12:20:29 +0800

    fix(zsh): copy themes to the wrong folder

--- zsh/install_zsh_plugins.sh
+++ zsh/install_zsh_plugins.sh
@@ -7,7 +7,7 @@ git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM
 git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # Add zsh themes
-cp -r themes/ ${ZSH/themes:-~/.oh-my-zsh/themes}
+cp -r themes/ "${ZSH:-~/.oh-my-zsh}"/themes
 
 # Install fzf plugin
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
